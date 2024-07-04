6e04307bc56bfa7933415dcac771eb21ca4353ab
Vishwa <vishwa.j1@ahduni.edu.in>
2024-07-04 14:22:10 +0530

    right side bar added and fixed some bugs

--- src/Component/Navbar/navbar.jsx
+++ src/Component/Navbar/navbar.jsx
@@ -7,63 +7,43 @@ import search from '../../assets/search-solid.svg'
 import Avatar from '../Avatar/Avatar'
 import './navbar.css'
 
-function navbar(handleslidein) {
+function navbar({handleslidein}) {
     var User=null; 
 
-return (
+  return (
     <nav className="main-nav">
-      <div className="navbar">
-        <button className="slide-in-icon" onClick={() => handleslidein()}>
-          <img src={bars} alt="bars" width="15" />
-        </button>
-        <div className="navbar-1">
-          <Link to="/" className="nav-item nav-logo">
-            <img src={logo} alt="logo" />
-          </Link>
-          <Link to="/" className="nav-item nav-btn res-nav">
-            About
-          </Link>
-          <Link to="/" className="nav-item nav-btn res-nav">
-            Products
-          </Link>
-          <Link to="/" className="nav-item nav-btn res-nav">
-            For Teams
-          </Link>
-          <form>
-            <input type="text" placeholder="Search..." />
-            <img src={search} alt="search" width="18" className="search-icon" />
-          </form>
+        <div className="navbar">
+            <button className="slide-in-icon" onClick={()=>handleslidein()}>
+                <img src={bars} alt="bars" width='15' />
+            </button>
+            <div className="navbar-1">
+                <Link to ='/' className='nav-item nav-logo'>
+                <img src={logo} alt="logo" width="120" /></Link>
+                <Link to='/' className='nav-item nav-btn res-nav'>
+                About</Link>
+                <Link to='/' className='nav-item nav-btn res-nav'>
+                Products</Link>
+                <Link to='/' className='nav-item nav-btn res-nav'>
+                For Teams</Link>
+                <form><input type="text" placeholder='Search...' />
+                <img src={search} alt="search" width='18' className='search-icon' /></form>
+            </div>
+            <div className="navbar-2">
+                {User==null ?(
+                    <Link to='/' className='nav-item nav-links'>
+                        Log In
+                    </Link>
+                ):(
+                    <>
+                    <Avatar backgroundColor='#009dff' px='10px' py='7px' borderRadius='50%' color='white'>
+                        <Link to='' style={{color:"white", textDecoration:"none"}}></Link>
+                    </Avatar>
+                    <button className="nav-item nav-links">Log Out</button></>
+                )}
+            </div>
         </div>
-        <div className="navbar-2">
-          {User === null ? (
-            <Link to="/Auth" className="nav-item nav-links">
-              Log in
-            </Link>
-          ) : (
-            <>
-              <Avatar
-                backgroundColor="#009dff"
-                px="10px"
-                py="7px"
-                borderRadius="50%"
-                color="white"
-              >
-                <Link
-                  to={`/Users/${User?.result?._id}`}
-                  style={{ color: "white", textDecoration: "none" }}
-                >
-                  {User.result.name.charAt(0).toUpperCase()}
-                </Link>
-              </Avatar>
-              <button className="nav-item nav-links" >
-                Log out
-              </button>
-            </>
-          )}
-        </div>
-      </div>
     </nav>
-  );
-};
+  )
+}
 
 export default navbar
